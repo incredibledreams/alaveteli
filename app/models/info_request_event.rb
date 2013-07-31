1213d52bbc20827ec7a6adf0cc1708f12a9e3d4a
Louise Crow <louise.crow@gmail.com>
2013-07-31 15:37:31 +0100

    Remove unused InfoRequestEvent.user_can_view? method.

--- app/models/info_request_event.rb
+++ app/models/info_request_event.rb
@@ -67,22 +67,6 @@ class InfoRequestEvent < ActiveRecord::Base
         end
     end
 
-    def user_can_view?(user)
-        unless info_request.user_can_view?(user)
-            raise "internal error, called user_can_view? on event when there is not permission to view entire request"
-        end
-
-        case prominence
-        when 'hidden'
-            User.view_hidden_requests?(user)
-        when 'requester_only'
-            info_request.is_owning_user?(user)
-        else
-            true
-        end
-    end
-
-
     # Full text search indexing
     acts_as_xapian :texts => [ :search_text_main, :title ],
         :values => [
